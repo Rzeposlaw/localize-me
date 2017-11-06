1a712498a486355acec2c3322c1e73842011afdc
Rzeposlaw <katarzyn.rzepecka@gmail.com>
2017-11-06 23:03:20 +0100

    Added GUI with viewpager to login and register module

---
+++ app/src/main/java/com/example/rzeposlaw/myapplication/LoginRegisterPagerAdapter.java
@@ -0,0 +1,40 @@
+package com.example.rzeposlaw.myapplication;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LoginRegisterPagerAdapter extends PagerAdapter {
+
+    private Context mContext;
+    private int[] layouts = {R.layout.view_pager_login, R.layout.view_pager_register};
+
+    public LoginRegisterPagerAdapter(Context context) {
+        mContext = context;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup collection, int position) {
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        ViewGroup layout = (ViewGroup) inflater.inflate(layouts[position], collection, false);
+        collection.addView(layout);
+        return layout;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup collection, int position, Object view) {
+        collection.removeView((View) view);
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+}
--- app/src/main/java/com/example/rzeposlaw/myapplication/SplashActivity.java
+++ app/src/main/java/com/example/rzeposlaw/myapplication/SplashActivity.java
@@ -1,7 +1,10 @@
 package com.example.rzeposlaw.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.splash);
+        linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SplashActivity.this, LoginRegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
