b8ed7392462c2ead1d86a7546b9cdc4c9d3f31b3
Rzeposlaw <katarzyn.rzepecka@gmail.com>
2017-11-09 21:11:03 +0100

    Replaced hardcoded strings, deleted unused imports

---
+++ app/src/main/java/com/example/rzeposlaw/localizeme/activity/LoginRegisterActivity.java
@@ -0,0 +1,75 @@
+package com.example.rzeposlaw.localizeme.activity;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.rzeposlaw.localizeme.adapter.LoginRegisterPagerAdapter;
+import com.example.rzeposlaw.localizeme.R;
+
+public class LoginRegisterActivity extends AppCompatActivity {
+
+    private Button loginButton;
+    private Button registerButton;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_register);
+
+        viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setAdapter(new LoginRegisterPagerAdapter(this));
+
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if(position == 0)
+                    loginButton.performClick();
+                else
+                    registerButton.performClick();
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        loginButton = (Button) findViewById(R.id.login);
+        registerButton = (Button) findViewById(R.id.register);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(0);
+                loginButton.setBackgroundDrawable(getResources()
+                        .getDrawable(R.drawable.buttonshapeleftblue));
+                registerButton.setBackgroundDrawable(getResources()
+                        .getDrawable(R.drawable.buttonshaperight));
+                loginButton.setTextColor(getResources().getColor(R.color.buttonTextColor));
+                registerButton.setTextColor(getResources().getColor(R.color.strokeButton));
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(1);
+                loginButton.setBackgroundDrawable(getResources()
+                        .getDrawable(R.drawable.buttonshapeleft));
+                registerButton.setBackgroundDrawable(getResources()
+                        .getDrawable(R.drawable.buttonshaperightblue));
+                registerButton.setTextColor(getResources().getColor(R.color.buttonTextColor));
+                loginButton.setTextColor(getResources().getColor(R.color.strokeButton));
+            }
+        });
+    }
+}
