3a8dfdca3c2f85b557945feaf28f82a91873ff0e
Rzeposlaw <katarzyn.rzepecka@gmail.com>
2018-01-17 23:26:41 +0100

    Added markers on map and drawing a polyline between locations

--- app/src/main/java/com/example/rzeposlaw/localizeme/activity/MapActivity.java
+++ app/src/main/java/com/example/rzeposlaw/localizeme/activity/MapActivity.java
@@ -9,10 +9,15 @@ import com.example.rzeposlaw.localizeme.adapter.RecyclerViewAdapter;
 import com.example.rzeposlaw.localizeme.data.ApiClient;
 import com.example.rzeposlaw.localizeme.data.Location;
 import com.example.rzeposlaw.localizeme.data.LocationAPI;
+import com.example.rzeposlaw.localizeme.utils.FetchUrl;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -23,7 +28,7 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     private LocationAPI apiService =
             ApiClient.getClient().create(LocationAPI.class);
 
-    private GoogleMap mMap;
+    private static GoogleMap mMap;
     private long loggedUserId;
     private long friendsId;
     private LatLng loggedUserLocation;
@@ -80,5 +85,48 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
 
             }
         });
+
+        if(loggedUserLocation != null && friendsLocation != null) {
+            MarkerOptions markerOptionsLogged = new MarkerOptions();
+            markerOptionsLogged.position(loggedUserLocation);
+            markerOptionsLogged.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker));
+            mMap.addMarker(markerOptionsLogged);
+
+            moveToCurrentLocation(loggedUserLocation);
+
+            MarkerOptions markerOptionsFriend = new MarkerOptions();
+            markerOptionsFriend.position(friendsLocation);
+            markerOptionsFriend.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker));
+            mMap.addMarker(markerOptionsFriend);
+            String url = getUrl(loggedUserLocation, friendsLocation);
+            FetchUrl FetchUrl = new FetchUrl();
+            FetchUrl.execute(url);
+        }
+    }
+
+    public static void addPolyline(PolylineOptions polylineOptions){
+        mMap.addPolyline(polylineOptions);
+    }
+
+    private String getUrl(LatLng origin, LatLng dest) {
+
+        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
+
+        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
+
+        String sensor = "sensor=false";
+
+        String parameters = str_origin + "&" + str_dest + "&" + sensor;
+
+        String url = "https://maps.googleapis.com/maps/api/directions/json?" + parameters;
+
+        return url;
+    }
+
+    private void moveToCurrentLocation(LatLng currentLocation)
+    {
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation,15));
+        mMap.animateCamera(CameraUpdateFactory.zoomIn());
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15), 2000, null);
     }
 }
