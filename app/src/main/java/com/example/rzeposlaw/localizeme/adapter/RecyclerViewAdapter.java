ec9cc5e74fe58cf693a04253827cc98977c5c503
Rzeposlaw <katarzyn.rzepecka@gmail.com>
2018-01-18 19:37:41 +0100

    Added avatars for users

--- app/src/main/java/com/example/rzeposlaw/localizeme/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/rzeposlaw/localizeme/adapter/RecyclerViewAdapter.java
@@ -12,6 +12,7 @@ import com.example.rzeposlaw.localizeme.R;
 import com.example.rzeposlaw.localizeme.activity.MapActivity;
 import com.example.rzeposlaw.localizeme.data.Credentials;
 import com.example.rzeposlaw.localizeme.view.RozhaOneTextView;
+import com.github.siyamed.shapeimageview.mask.PorterImageView;
 
 import java.util.ArrayList;
 
@@ -26,18 +27,23 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     private Long friendsId;
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
+
         public RozhaOneTextView username;
         public ImageButton openMapButton;
+        public RozhaOneTextView email;
+        public PorterImageView userAvatar;
 
         public ViewHolder(View view) {
             super(view);
             username = (RozhaOneTextView) view.findViewById(R.id.username);
             openMapButton = (ImageButton) view.findViewById(R.id.open_map);
+            email = (RozhaOneTextView) view.findViewById(R.id.email);
+            userAvatar = (PorterImageView) view.findViewById(R.id.user_avatar);
         }
     }
 
-    public RecyclerViewAdapter(ArrayList<Credentials> mDatasetUsernames, long loggedUserId, Context context) {
-        this.mDatasetUsers = mDatasetUsernames;
+    public RecyclerViewAdapter(ArrayList<Credentials> mDatasetUsers, long loggedUserId, Context context) {
+        this.mDatasetUsers = mDatasetUsers;
         this.loggedUserId = loggedUserId;
         mContext = context;
     }
@@ -60,6 +66,13 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
                 openMapActivity();
             }
         });
+        holder.email.setText(mDatasetUsers.get(position).getEmail());
+        if(mDatasetUsers.get(position).getSex().equals("W")){
+            holder.userAvatar.setImageDrawable(mContext.getResources().getDrawable(R.drawable.woman_avatar));
+        }
+        else{
+            holder.userAvatar.setImageDrawable(mContext.getResources().getDrawable(R.drawable.man_avatar));
+        }
     }
 
     private void openMapActivity(){
